705da555fd6bfc20a8640b3041e1fb6a8acb94ee
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2024-03-17 21:29:00 +0200

    update eks-tf dir

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    bucket = "enkidu1234bucket"
+    key    = "EKS-tf/terraform.tfstate"
     region = "ap-south-1"
   }
 }
